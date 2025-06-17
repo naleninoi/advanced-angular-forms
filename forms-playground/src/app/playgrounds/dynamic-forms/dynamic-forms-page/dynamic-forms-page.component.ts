f5d70bb5efec8081c3812bee000d36c83bfb73e0
viktor.kochetov-arkhipov <viktor.kochetov-arkhipov@sms-a.ru>
2025-06-17 16:57:01 +0300

    82 - Render the Form From Config

--- forms-playground/src/app/playgrounds/dynamic-forms/dynamic-forms-page/dynamic-forms-page.component.ts
+++ forms-playground/src/app/playgrounds/dynamic-forms/dynamic-forms-page/dynamic-forms-page.component.ts
@@ -1,30 +1,48 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Observable, Subject, switchMap } from 'rxjs';
+import { Observable, Subject, switchMap, tap } from 'rxjs';
+import { DynamicFormConfig } from '../dynamic-forms.model';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-dynamic-forms-page',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './dynamic-forms-page.component.html',
-  styleUrls: [
-    '../../common-page.scss',
-    './dynamic-forms-page.component.scss',
-  ],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-dynamic-forms-page',
+    standalone: true,
+    imports: [CommonModule, ReactiveFormsModule],
+    templateUrl: './dynamic-forms-page.component.html',
+    styleUrls: [
+        '../../common-page.scss',
+        './dynamic-forms-page.component.scss',
+    ],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DynamicFormsPageComponent implements OnInit {
 
-  protected formLoadingTrigger = new Subject<'user' | 'company'>();
-  protected formConfig$!: Observable<object>;
+    form!: FormGroup;
 
-  constructor(private http: HttpClient) { }
+    protected formLoadingTrigger = new Subject<'user' | 'company'>();
+    protected formConfig$!: Observable<DynamicFormConfig>;
 
-  ngOnInit(): void {
-    this.formConfig$ = this.formLoadingTrigger.pipe(
-      switchMap(config => this.http.get(`assets/${config}.form.json`))
-    );
-  }
+    constructor(private http: HttpClient) { }
+
+    ngOnInit(): void {
+        this.formConfig$ = this.formLoadingTrigger.pipe(
+            switchMap(config => this.http.get<DynamicFormConfig>(`assets/${config}.form.json`)),
+            tap(({ controls }) => this.buildForm(controls))
+        );
+    }
+
+    public onSubmit(): void {
+        console.log(this.form.value);
+        this.form.reset();        
+    }
+
+    private buildForm(controls: DynamicFormConfig['controls']): void {
+        this.form = new FormGroup({});
+
+        Object.keys(controls).forEach(key => {
+            this.form.addControl(key, new FormControl(controls[key].value))
+        });        
+    }
 
 }
