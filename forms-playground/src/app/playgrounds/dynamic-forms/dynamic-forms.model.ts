604760e399b84ab59de99af9429a5fdd2b51a137
viktor.kochetov-arkhipov <viktor.kochetov-arkhipov@sms-a.ru>
2025-06-17 14:59:33 +0300

    81 - Create JSON Config & Corresponding Interfaces

---
+++ forms-playground/src/app/playgrounds/dynamic-forms/dynamic-forms.model.ts
@@ -0,0 +1,19 @@
+export interface DynamicOptions {
+    label: string;
+    value: string;
+}
+
+export interface DynamicControl<T = string> {
+    controlType: 'input' | 'select';
+    type?: string;
+    label: string;
+    value: T | null;
+    options? : DynamicOptions[];
+}
+
+export interface DynamicFormConfig {
+    description: string;
+    controls: {
+        [key: string]: DynamicControl
+    }
+}
